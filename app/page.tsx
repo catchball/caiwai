bef63df783212f5c849433df9cb318afe375e773
kixixixixi <kixixixixi@gmail.com>
2025-09-02 19:13:39 +0900

    Add top page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,51 @@
-import React, { FC } from "react"
+import Link from "next/link"
+import React, { FC, Suspense, use } from "react"
+import { api } from "services/api"
 
 const Page: FC = ({ ...props }) => {
-  return <React.Fragment {...props}></React.Fragment>
+  return (
+    <React.Fragment {...props}>
+      <Suspense fallback={<>Loading</>}>
+        <Projects />
+      </Suspense>
+    </React.Fragment>
+  )
+}
+
+const Projects: FC = () => {
+  const fetchProjects = async () => {
+    const { projects } = await api.v1.projectsApiV1ClippingsProjectsGet({
+      status: "Active",
+    })
+
+    return projects.filter((p) => p.has_scheduled_export)
+  }
+  const projects = use(fetchProjects())
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: ".5rem",
+        padding: "1rem",
+      }}
+    >
+      {projects.map((project) => (
+        <Link
+          key={project.id}
+          href={`/project/${project.id}`}
+          style={{
+            background: "#f9f9f9",
+            borderRadius: "2rem",
+            display: "block",
+            padding: ".25rem .75rem",
+          }}
+        >
+          {project.name}
+        </Link>
+      ))}
+    </div>
+  )
 }
 
 export default Page
