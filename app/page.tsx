3984672efbd7318707ffbb9059df33b343d68111
kixixixixi <kixixixixi@gmail.com>
2025-10-02 19:45:32 +0900

    Add menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 import React, { FC } from "react"
 
+const MenuLink: FC<{ href: string; children: React.ReactNode }> = ({
+  href,
+  children,
+  ...props
+}) => (
+  <a
+    href={href}
+    style={{
+      background: "#f0f0f0",
+      border: "solid 1px #ccc",
+      borderRadius: ".5rem",
+      boxShadow: ".5rem .5rem .5rem rgba(0,0,0,.1)",
+      padding: "1rem",
+    }}
+    {...props}
+  >
+    {children}
+  </a>
+)
+
 const Page: FC = ({ ...props }) => {
   return (
     <React.Fragment {...props}>
       <div style={{ padding: 0 }}>
-        <div
+        <a
+          href="//docs.google.com/presentation/d/19kgtO1xAhNhkH_rbMM5tKVgPyXwJj8Suhy32oZx0YdY/edit?usp=sharing"
           style={{
             alignItems: "center",
             background: "#f0f0f0",
@@ -22,7 +43,12 @@ const Page: FC = ({ ...props }) => {
           >
             界隈ごと、まるわかり。
           </h2>
-        </div>
+        </a>
+      </div>
+      <div style={{ display: "flex", padding: "1rem" }}>
+        <MenuLink href="//sites.google.com/catch-ball.co.jp/caiwai-magazinelist/">
+          マガジンリストをみる
+        </MenuLink>
       </div>
     </React.Fragment>
   )
