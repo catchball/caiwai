2a9ce240c2b7ad8f8247d85f67e3bd535e5c0e0b
kixixixixi <kixixixixi@gmail.com>
2026-03-26 18:37:49 +0900

    Fix project api

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+
+import { ClippingProject } from "@catchball/tansaku-client/lib"
 import { PrivateLock } from "components/commons/private-lock"
+import { useAtomValue } from "jotai"
 import Link from "next/link"
-import React, { FC, Suspense, use } from "react"
+import React, { FC, Suspense, useEffect, useState } from "react"
 import { api } from "services/api"
+import { userAtom } from "services/store"
 
 const Page: FC = ({ ...props }) => {
   return (
@@ -16,36 +21,44 @@ const Page: FC = ({ ...props }) => {
 }
 
 const Projects: FC = () => {
-  const fetchProjects = async () => {
-    const { projects } = await api.v1.projectsApiV1ProjectsProjectsGet()
+  const [projects, setProjects] = useState<ClippingProject[]>([])
+  const user = useAtomValue(userAtom)
 
-    return projects.filter((p) => p.has_scheduled_export)
-  }
-  const projects = use(fetchProjects())
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const { projects } = await api.v1.projectsApiV1ProjectsGet()
+      setProjects(projects)
+    }
+    if (user) fetchProjects()
+  }, [user])
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        gap: ".5rem",
-        padding: "1rem",
-      }}
-    >
-      {projects.map((project) => (
-        <Link
-          key={project.id}
-          href={`/project/${project.id}/compact`}
-          style={{
-            background: "#f9f9f9",
-            borderRadius: "2rem",
-            display: "block",
-            padding: ".25rem .75rem",
-          }}
-        >
-          {project.name}
-        </Link>
-      ))}
-    </div>
+    <>
+      <div>{user.displayName}</div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: ".5rem",
+          padding: "1rem",
+        }}
+      >
+        {projects.map((project) => (
+          <Link
+            key={project.id}
+            href={`/project/${project.id}`}
+            style={{
+              background: "#f9f9f9",
+              borderRadius: "2rem",
+              display: "block",
+              padding: ".25rem .75rem",
+            }}
+          >
+            {project.name}
+          </Link>
+        ))}
+      </div>
+    </>
   )
 }
 
--- services/api.ts
+++ services/api.ts
@@ -1,4 +1,5 @@
 import { ApiClient } from "@catchball/tansaku-client/lib"
+import { auth } from "./firebase"
 
 const BASE =
   process.env.NEXT_PUBLIC_API_BASE ||
@@ -6,4 +7,8 @@ const BASE =
 
 export const api = new ApiClient({
   BASE,
+  HEADERS: async () => {
+    const token = await auth.currentUser?.getIdToken()
+    return token ? { Authorization: `Bearer ${token}` } : {}
+  },
 })
