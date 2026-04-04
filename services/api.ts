517640a47e33ce935176377ac3f7e02859fdfc7c
kixixixixi <kixixixixi@gmail.com>
2026-04-04 21:51:55 +0900

    Fix api host

--- services/api.ts
+++ services/api.ts
@@ -3,7 +3,7 @@ import { auth } from "./firebase"
 
 const BASE =
   process.env.NEXT_PUBLIC_API_BASE ||
-  ("https://api.saku2.reload.co.jp" as const)
+  ("https://tansaku.catch-ball.co.jp" as const)
 
 export const api = new ApiClient({
   BASE,
